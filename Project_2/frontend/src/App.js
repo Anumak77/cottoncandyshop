6a6640096205270c01b5e842de73d61a680da908
Anushree Umak <anushree.umak2@mail.dcu.ie>
2023-04-19 23:19:21 +0100

    removed things that didnt work

--- Project_2/frontend/src/App.js
+++ Project_2/frontend/src/App.js
@@ -1,26 +1,9 @@
 
 import './App.css';
 
-// import DegreeFacts from './components/degreelist';
-// import DegreeId from './components/degreeid'
-// // import Degreeadd from './components/degreecreate';
 
-// import CohortFacts from './components/cohortlist';
-// // import CohortId from './components/cohortid';
-// // import Cohortadd from './components/cohortcreate';
-// import ModulesFacts from './components/moduleslist';
-// import ModuleId from './components/modulesid';
-// import ModulecohortId from './components/modulecohort';
-// import Moduleadd from './components/modulecreate';
-// import StudentId from './components/studentid';
-// import Studentadd from './components/studentcreate';
-// import Gradeadd from './components/gradeadd';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
+import Initial from './components/together';
 
-import First from './components/together';
-// import Add from './components/create';
-// import NavigationBar from './components/navbar';
 
 
 function App() {
@@ -29,49 +12,11 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-      {/* <NavigationBar /> */}
-      {/* <DegreeId id="COMSCI" /> */}
-      {/* <DegreeFacts/>
-      <CohortFacts/>
-      <ModulesFacts/> */}
-            <br></br>
+
+        <br></br>
         <br></br>
 
-        <First/>
-        {/* <Add/> */}
-{/* 
-        <DegreeFacts/>
-        <DegreeId id="COMSCI" />
-        <Degreeadd/>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br> */}
-        {/* <CohortFacts/>
-        <CohortId id="COMSCI1"/>
-        <Cohortadd/>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <ModulesFacts/>
-        <ModuleId id="CA298"/>
-        <ModulecohortId id="COMSCI2"/>
-        <Moduleadd/>
-        
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <StudentId id="26870106"/>
-        <Studentadd/>
-        <Gradeadd/> */}
-
-        
-      
+        <Initial/>
 
       </header>
     </div>
